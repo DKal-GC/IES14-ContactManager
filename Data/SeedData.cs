af5633b4c3d2dadb25035c97b0123439e210beb6
Dhanuja <Dhanuja@DESKTOP-Q6I5E3L>
2022-12-02 10:29:18 -0500

    Add project files.

---
+++ Data/SeedData.cs
@@ -0,0 +1,164 @@
+﻿using ContactManager.Authorization;
+using ContactManager.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+// dotnet aspnet-codegenerator razorpage -m Contact -dc ApplicationDbContext -outDir Pages\Contacts --referenceScriptLibraries
+namespace ContactManager.Data
+{
+    public static class SeedData
+    {
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+        #region snippet_Initialize
+        public static async Task Initialize(IServiceProvider serviceProvider, string testUserPw)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                // For sample purposes seed both with the same password.
+                // Password is set with the following:
+                // dotnet user-secrets set SeedUserPW <pw>
+                // The admin user can do anything
+
+                var adminID = await EnsureUser(serviceProvider, testUserPw, "admin@ies14.com");
+                await EnsureRole(serviceProvider, adminID, Constants.ContactAdministratorsRole);
+
+                // allowed user can create and edit contacts that they create
+                var managerID = await EnsureUser(serviceProvider, testUserPw, "manager@ies14.com");
+                await EnsureRole(serviceProvider, managerID, Constants.ContactManagersRole);
+
+                SeedDB(context, adminID);
+            }
+        }
+
+        private static async Task<string> EnsureUser(IServiceProvider serviceProvider,
+                                                    string testUserPw, string UserName)
+        {
+            var userManager = serviceProvider.GetService<UserManager<IdentityUser>>();
+
+            var user = await userManager.FindByNameAsync(UserName);
+            if (user == null)
+            {
+                user = new IdentityUser
+                {
+                    UserName = UserName,
+                    EmailConfirmed = true
+                };
+                await userManager.CreateAsync(user, testUserPw);
+            }
+
+            if (user == null)
+            {
+                throw new Exception("The password is probably not strong enough!");
+            }
+
+            return user.Id;
+        }
+
+        private static async Task<IdentityResult> EnsureRole(IServiceProvider serviceProvider,
+                                                                      string uid, string role)
+        {
+            var roleManager = serviceProvider.GetService<RoleManager<IdentityRole>>();
+
+            if (roleManager == null)
+            {
+                throw new Exception("roleManager null");
+            }
+
+            IdentityResult IR;
+            if (!await roleManager.RoleExistsAsync(role))
+            {
+                IR = await roleManager.CreateAsync(new IdentityRole(role));
+            }
+
+            var userManager = serviceProvider.GetService<UserManager<IdentityUser>>();
+
+            //if (userManager == null)
+            //{
+            //    throw new Exception("userManager is null");
+            //}
+
+            var user = await userManager.FindByIdAsync(uid);
+
+            if (user == null)
+            {
+                throw new Exception("The testUserPw password was probably not strong enough!");
+            }
+
+            IR = await userManager.AddToRoleAsync(user, role);
+
+            return IR;
+        }
+        #endregion
+        #region snippet1
+        public static void SeedDB(ApplicationDbContext context, string adminID)
+        {
+            if (context.Contact.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Contact.AddRange(
+            #region snippet_Contact
+                new Contact
+                {
+                    Name = "Asami Sato",
+                    Address = "3085 St Jean Baptiste St",
+                    City = "Ham Nord",
+                    State = "Quebec",
+                    Zip = "G0P 1A0",
+                    Email = "asami.s@ies14.com",
+                    Status = ContactStatus.Approved,
+                    OwnerID = adminID
+                },
+            #endregion
+            #endregion
+                new Contact
+                {
+                    Name = "James Bond",
+                    Address = "982 Lynden Road",
+                    City = "Cavan",
+                    State = "Ontario",
+                    Zip = "L0A 1C0",
+                    Email = "james.b@ies14.com",
+                    Status = ContactStatus.Submitted,
+                    OwnerID = adminID
+                },
+                new Contact
+                {
+                    Name = "James Mcgill",
+                    Address = "231 Bridgeport Rd",
+                    City = "Orangeville",
+                    State = "Ontario",
+                    Zip = "L9W 2C8",
+                    Email = "james.m@ies14.com",
+                    Status = ContactStatus.Rejected,
+                    OwnerID = adminID
+                },
+                new Contact
+                {
+                    Name = "Morty Smith",
+                    Address = "2623 Brew Creek Rd",
+                    City = "Port Mellon",
+                    State = "British Columbia",
+                    Zip = "V0N 2S0",
+                    Email = "morty.s@ies14.com",
+                    Status = ContactStatus.Submitted,
+                    OwnerID = adminID
+                },
+                new Contact
+                {
+                    Name = "Michael Scott",
+                    Address = "388 Campsite Road",
+                    City = "Spruce Grove",
+                    State = "Alberta",
+                    Zip = "T7X 2Y8",
+                    Email = "michael.s@ies14.com",
+                    OwnerID = adminID
+                }
+             );
+            context.SaveChanges();
+        }
+    }
+}
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
